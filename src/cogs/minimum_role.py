9d2f1778215bc804e195a333e593a795b1eefd6e
RRR-09 <79768142+RRR-09@users.noreply.github.com>
2022-02-21 21:50:58 -0500

    temp role functionality

--- src/cogs/minimum_role.py
+++ src/cogs/minimum_role.py
@@ -23,18 +23,23 @@ class MinimumRole(commands.Cog):
 
         self.check_members_have_minimum_role.start()
 
+    async def check_member_has_minimum_role(self, member, do_warn=True):
+        if len(member.roles) >= 2:  # Ignore users with 2 roles or more, likely fine
+            return
+
+        if (
+            self.log_channel is not None and do_warn
+        ):  # Log that someone was missing a role
+            await self.log_channel.send(
+                f"Warning: {member.mention} missing Guest role. Adding."
+            )
+
+        await member.add_roles(self.minimum_role)
+
     @tasks.loop(seconds=30)
     async def check_members_have_minimum_role(self):
         async for member in self.bot.guild.fetch_members(limit=None):
-            if len(member.roles) >= 2:  # Ignore users with 2 roles or more, likely fine
-                continue
-
-            if self.log_channel is not None:  # Log that someone was missing a role
-                await self.log_channel.send(
-                    f"Warning: {member.mention} missing Guest role. Adding."
-                )
-
-            await member.add_roles(self.minimum_role)
+            await self.check_member_has_minimum_role(member)
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
--- src/cogs/store.py
+++ src/cogs/store.py
@@ -1,11 +1,13 @@
 import json
 from pathlib import Path
-from typing import Any, Dict, List
+from time import time
+from traceback import format_exc
+from typing import Any, Dict, List, Union
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
-from utils import BotClass, do_log, get_est_time, log_error
+from utils import BotClass, do_log, get_est_time, json_load_eval, log_error
 
 
 class Store(commands.Cog):
@@ -27,8 +29,20 @@ class Store(commands.Cog):
             return
 
         data_file_name = "store_temporary_purchases.json"
-        data_folder_path = Path.cwd() / "data"
-        self.temp_purchases_data_file_path = data_folder_path / data_file_name
+        self.temp_purchases_data_folder_path = Path.cwd() / "data"
+        self.temp_purchases_data_file_path = (
+            self.temp_purchases_data_folder_path / data_file_name
+        )
+
+        # Load the datafile
+        self.temp_purchases = {}
+        try:
+            with open(self.temp_purchases_data_file_path, "r") as json_file:
+                self.temp_purchases = json_load_eval(json_file)
+        except FileNotFoundError:
+            Path(self.temp_purchases_data_folder_path).mkdir(exist_ok=True)
+            with open(self.temp_purchases_data_file_path, "w") as json_file:
+                json.dump(self.temp_purchases, json_file, indent=4)
 
         self.rcon_function = self.bot.client.get_cog(
             "MinecraftIntegration"
@@ -44,6 +58,12 @@ class Store(commands.Cog):
         self.discord_to_minecraft = self.bot.client.get_cog(
             "MinecraftIntegration"
         ).discord_to_minecraft
+
+        self.check_member_has_minimum_role = self.bot.client.get_cog(
+            "MinimumRole"
+        ).check_member_has_minimum_role
+
+        self.remove_temp_roles.start()
 
     async def log_transaction(self, transaction_obj: Dict):
         buy_time = get_est_time()
@@ -76,6 +96,16 @@ class Store(commands.Cog):
 
         self.rcon_function(cmds=commands_to_run)
 
+    def log_temp_roles(
+        self, discord_id: int, temp_roles: List[Dict[str, Union[int, float]]]
+    ):
+        entry = self.temp_purchases.get(discord_id, [])
+        entry = entry + temp_roles
+        self.temp_purchases[discord_id] = entry
+
+        with open(self.temp_purchases_data_file_path, "w") as json_file:
+            json.dump(self.temp_purchases, json_file, indent=4)
+
     async def give_discord_roles(self, transaction_obj: Dict):
         roles: List[Dict] = transaction_obj.get("item", {}).get("discord_roles", None)
         if roles is None:
@@ -93,7 +123,7 @@ class Store(commands.Cog):
                 f"Could not find {user_name}'s discord, but they bought something with a role!"
             )
             return
-        user_discord = self.bot.guild.get_member(discord_id)
+        user_discord = self.bot.guild.get_member(user_discord_id)
         if user_discord is None:
             await self.error_log_channel.send(
                 f"{user_name} isn't in the discord anymore, but they bought something with a role!"
@@ -102,6 +132,8 @@ class Store(commands.Cog):
 
         roles_to_add = []
         roles_to_remove = []
+        temp_roles = []
+        current_time = time()
         for role in roles:
             role_name = role.get("name", "role_name_not_found")
             role_instance = self.bot.roles.get(role_name)
@@ -116,6 +148,14 @@ class Store(commands.Cog):
                 # TODO: Daily role check to remove temp roles
                 days = role.get("duration_days", -1)
                 if days != -1:
+                    temp_roles.append(
+                        {
+                            "role_id": role_instance.id,
+                            "expiry_timestamp": round(
+                                current_time + (days * 24 * 60 * 60)
+                            ),
+                        }
+                    )
                     await self.error_log_channel.send(
                         f"{user_name} ({user_discord.name}#{user_discord.discriminator}) has purchased a temporary "
                         f"role ({role_instance.name} for {days} days).\nAutomatic role strip, or notification if "
@@ -124,11 +164,71 @@ class Store(commands.Cog):
 
             else:
                 roles_to_remove.append(role_instance)
-
+        if temp_roles:
+            self.log_temp_roles(user_discord.id, temp_roles)
         if roles_to_add:
             await user_discord.add_roles(*roles_to_add)
         if roles_to_remove:
             await user_discord.remove_roles(*roles_to_remove)
+
+    @tasks.loop(seconds=60)
+    async def remove_temp_roles(self):
+        current_time = time()
+
+        for discord_id in self.temp_purchases:
+            discord_id = int(discord_id)
+            try:
+                member = self.bot.guild.get_member(discord_id)
+            except Exception:
+                do_log(
+                    f"[remove_temp_roles] Member with discord id '{discord_id}' not found"
+                )
+                continue
+            temp_roles = self.temp_purchases[discord_id]
+            remaining_roles = []
+            for role_entry in temp_roles:
+                try:
+                    expiry_timestamp = int(role_entry["expiry_timestamp"])
+                except Exception:
+                    log_error(
+                        f"[Temp Role] Could not get expiry_timestamp for discord id '{discord_id}'\n"
+                        f"{temp_roles}\n"
+                    )
+                    continue
+                if expiry_timestamp > current_time:
+                    remaining_roles.append(role_entry)
+                    continue
+                try:
+                    role_id = int(role_entry["role_id"])
+                    role_instance = self.bot.guild.get_role(role_id)
+                    if role_instance is None:
+                        raise Exception(
+                            f"Could not find role on discord by id '{role_id}'"
+                        )
+                    if member is None:
+                        raise Exception(
+                            f"Could not find member on discord by id '{discord_id}'"
+                        )
+                    await member.remove_roles(
+                        role_instance, reason="Temporary Purchased Role Expired"
+                    )
+                except Exception as e:
+                    log_error(f"[remove_temp_roles]\n{format_exc()}\n")
+
+                    await self.error_log_channel.send(
+                        f"Could not remove temporary role from discord id '{discord_id}', check bot error logs\n`{e}`"
+                    )
+                    continue
+
+                await self.error_log_channel.send(
+                    f"Removed temporary role '{role_instance.name}' from discord member {member.mention}"
+                )
+
+            self.temp_purchases[discord_id] = remaining_roles
+            await self.check_member_has_minimum_role(member, do_warn=False)
+
+        with open(self.temp_purchases_data_file_path, "w") as json_file:
+            json.dump(self.temp_purchases, json_file, indent=4)
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
